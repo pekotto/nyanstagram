5f0048e09e8ec42e07540af5f92bb2b9a2d8acba
vagrant <vagrant@localhost.localdomain>
2020-06-23 06:12:53 +0000

    [Add]DM index

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,14 @@
 class RoomsController < ApplicationController
 
+  def index
+    @currentEntries = current_user.entries
+    myRoomIds = []
+    @currentEntries.each do | entry |
+      myRoomIds << entry.room.id
+    end
+    @anotherEntries = Entry.where(room_id: myRoomIds).where('user_id != ?', current_user.id)
+  end
+
   def create
     @room = Room.create
     @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   end
 
   resources :messages, only: [:create]
-  resources :rooms, only: [:create, :show]
+  resources :rooms, only: [:index, :create, :show]
 
   resources :relationships, only: [:create, :destroy]
 
